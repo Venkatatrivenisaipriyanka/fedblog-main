0f618ddd028f8ad0f05df8d531df9ad29dcd3aa0
Venkatatrivenisaipriyanka <109616800+Venkatatrivenisaipriyanka@users.noreply.github.com>
2022-10-11 17:55:46 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
       <div className="footer">
         <div className="footer-content">
             <h3>My FED Blog</h3>
+            <h6>20bq1a05f6</h6>
             <p>copyright &copy;2021 <a href="#"> My FED Blog</a>  </p>
         </div>
       </div>
